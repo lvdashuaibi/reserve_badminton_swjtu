34880614aebfd9ce3de162f7c2064aae5a87a164
lvdashuaibi <2608485769@qq.com>
2024-12-01 22:58:59 +0800

    将密码放在环境变量中

--- serverf/midware/db.go
+++ serverf/midware/db.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"errors"
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"os"
 	"sync"
 )
 
@@ -16,23 +18,74 @@ type User struct {
 	Email    string `gorm:"email"`
 }
 
+// DatabaseConfig 用来存储数据库配置信息
+type DatabaseConfig struct {
+	Username  string
+	Password  string
+	Host      string
+	Port      int
+	DBName    string
+	Charset   string
+	ParseTime bool
+	Loc       string
+}
+
 var (
 	db     *gorm.DB
 	dbOnce sync.Once
 )
+
+func loadConfig() (*DatabaseConfig, error) {
+	// 读取环境变量中的密码
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		return nil, fmt.Errorf("环境变量 DB_PASSWORD 未设置")
+	}
+
+	// 配置数据库连接信息
+	config := &DatabaseConfig{
+		Username:  "root",
+		Password:  password,
+		Host:      "127.0.0.1",
+		Port:      3306,
+		DBName:    "badminton",
+		Charset:   "utf8mb4",
+		ParseTime: true,
+		Loc:       "Local",
+	}
+
+	return config, nil
+}
 
 func init() {
 	openDB()
 }
 
 func openDB() {
-	var err error
-	dsn := "root:100617YYzyq.@tcp(127.0.0.1:3306)/badminton?charset=utf8mb4&parseTime=True&loc=Local"
+	// 从配置文件加载数据库配置信息
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatalf("加载配置失败: %v", err)
+		return
+	}
+
+	// 使用配置文件中的信息构建DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=%t&loc=%s",
+		config.Username, config.Password, config.Host, config.Port, config.DBName, config.Charset, config.ParseTime, config.Loc)
+
+	// 打开数据库连接
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Printf("连接数据库失败:%v", err)
+		log.Printf("连接数据库失败: %v", err)
+		return
 	}
-	db.AutoMigrate(&User{})
+
+	// 自动迁移
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		log.Printf("数据库自动迁移失败%v\n", err)
+		return
+	}
 	//fmt.Println("数据库连接成功")
 }
 
