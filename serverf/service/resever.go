e39a4310cbcfe91572ca0ccdf59df29283743052
lvdashuaibi <2608485769@qq.com>
2024-12-23 23:10:55 +0800

    一次预定两个小时

--- serverf/service/resever.go
+++ serverf/service/resever.go
@@ -24,6 +24,23 @@ func Reservation(c *gin.Context, req entity.GetSessionIDRequest) (interface{}, e
 
 	// 获取场次ID
 	sessionID, err := getSessionID(token, entity.FieldID, req.Date, req.TimeSlot, req.CourtNum+"号羽毛球", entity.SportTypeID)
+	//fmt.Println(req.TimeSlot)
+
+	//同时预定下一个小时
+	timeStr := req.TimeSlot
+	// 解析为时间类型
+	t, err := time.Parse("15:04:05", timeStr)
+	if err != nil {
+		fmt.Println("Error parsing time:", err)
+	}
+
+	// 增加一小时
+	t = t.Add(time.Hour)
+
+	// 格式化为字符串
+	newTimeStr := t.Format("15:04:05")
+	//fmt.Println("Updated Time:", newTimeStr)
+	sessionID2, err := getSessionID(token, entity.FieldID, req.Date, newTimeStr, req.CourtNum+"号羽毛球", entity.SportTypeID)
 	if err != nil {
 		return nil, err
 	}
@@ -42,7 +59,7 @@ func Reservation(c *gin.Context, req entity.GetSessionIDRequest) (interface{}, e
 			Date     string
 			TimeSlot string
 			CourtNum string
-		}(req))
+		}(req), sessionID2)
 	} else {
 		// 等待到22:30再发起预定请求
 		time.AfterFunc(nextReservationTime.Sub(now), func() {
@@ -50,7 +67,7 @@ func Reservation(c *gin.Context, req entity.GetSessionIDRequest) (interface{}, e
 				Date     string
 				TimeSlot string
 				CourtNum string
-			}(req))
+			}(req), sessionID2)
 		})
 	}
 	return nil, nil
@@ -116,9 +133,9 @@ func reserveCourtAtTime(token string, sessionID string, username string, req str
 	Date     string
 	TimeSlot string
 	CourtNum string
-}) {
+}, sessionID2 string) {
 	// 预定场地
-	err := reserveCourt(token, sessionID, entity.FieldID, req.Date, req.CourtNum+"号羽毛球", entity.SportTypeID, username, req.TimeSlot)
+	err := reserveCourt(token, sessionID, entity.FieldID, req.Date, req.CourtNum+"号羽毛球", entity.SportTypeID, username, req.TimeSlot, sessionID2)
 	if err != nil {
 		// 记录失败的日志或通知管理员
 		return
@@ -126,7 +143,7 @@ func reserveCourtAtTime(token string, sessionID string, username string, req str
 	// 记录成功的预定操作
 }
 
-func reserveCourt(token, sessionID, fieldID, date, courtName, sportTypeID, username, timeSlot string) error {
+func reserveCourt(token, sessionID, fieldID, date, courtName, sportTypeID, username, timeSlot, sessionID2 string) error {
 	// 定义最大重试时间为2小时
 	retryDuration := 3 * time.Hour
 	// 定义每次重试的间隔时间，例如每1分钟重试一次
@@ -147,7 +164,7 @@ func reserveCourt(token, sessionID, fieldID, date, courtName, sportTypeID, usern
 	requestBody := entity.ReserveRequest{
 		Number:        3,
 		OrderUseDate:  fmt.Sprintf("%d", timestamp), // 传递时间戳
-		RequestsList:  []entity.ReserveSession{{SessionsID: sessionID}},
+		RequestsList:  []entity.ReserveSession{{SessionsID: sessionID}, {SessionsID: sessionID2}},
 		FieldName:     "犀浦室内羽毛球馆",
 		FieldID:       fieldID,
 		SiteName:      courtName,
@@ -183,10 +200,10 @@ func reserveCourt(token, sessionID, fieldID, date, courtName, sportTypeID, usern
 
 		// 检查是否是 "预定日期未开放" 错误
 		if strings.Contains(string(bodyBytes), "预定日期未开放，请勿超期预定") {
-			log.Printf("%s号，时间：%s ：预定日期未开放，等待重试...", courtName, timeSlot)
+			log.Printf("%s，时间：%s ：预定日期未开放，等待重试...", courtName, timeSlot)
 			time.Sleep(retryInterval) // 等待retryInterval后重试
 		} else if strings.Contains(string(bodyBytes), "已被占用") {
-			log.Printf("\"%s号，时间：%s ：显示已被占用，等待重试...", courtName, timeSlot)
+			log.Printf("\"%s，时间：%s ：显示已被占用，等待重试...", courtName, timeSlot)
 			time.Sleep(retryInterval) // 等待retryInterval后重试
 		} else if strings.Contains(string(bodyBytes), "200") {
 			// 设置邮件正文
@@ -194,12 +211,98 @@ func reserveCourt(token, sessionID, fieldID, date, courtName, sportTypeID, usern
 			if err != nil {
 				log.Println("邮件发送失败")
 			}
-			log.Printf("\"%s号，时间：%s ：预定成功!", courtName, timeSlot)
+			log.Printf("\"%s，时间：%s ：预定成功!", courtName, timeSlot)
 			return nil
 		} else {
 			// 如果是其他错误，直接返回
-			return fmt.Errorf("预定失败: %s", string(bodyBytes))
+			log.Printf("Error:%s", string(bodyBytes))
 		}
+
+		fmt.Println(string(bodyBytes))
+
+		retryInterval += time.Second
+
+	}
+
+	return fmt.Errorf("超过最大重试时间，预定失败")
+}
+
+func reserveCourt2(token, sessionID, fieldID, date, courtName, sportTypeID, username, timeSlot, sessionID2 string) error {
+	// 定义最大重试时间为2小时
+	retryDuration := 3 * time.Hour
+	// 定义每次重试的间隔时间，例如每1分钟重试一次
+	retryInterval := time.Second * 3
+	// 开始重试的时间
+	startTime := time.Now()
+
+	//log.Println("尝试预定...")
+
+	// 将日期转换为时间戳
+	layout := "2006-01-02"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		return fmt.Errorf("日期解析失败: %v", err)
+	}
+	timestamp := t.UnixMilli() // 转换为毫秒时间戳
+
+	requestBody := entity.ReserveRequest{
+		Number:        3,
+		OrderUseDate:  fmt.Sprintf("%d", timestamp), // 传递时间戳
+		RequestsList:  []entity.ReserveSession{{SessionsID: sessionID}, {SessionsID: sessionID2}},
+		FieldName:     "犀浦室内羽毛球馆",
+		FieldID:       fieldID,
+		SiteName:      courtName,
+		SportTypeName: "羽毛球",
+		SportTypeID:   sportTypeID,
+	}
+
+	jsonBody, _ := json.Marshal(requestBody)
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s%s?timestamp=%d", entity.BaseURL, entity.ReserveSessionPath, timestamp), bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return fmt.Errorf("请求创建失败: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-UserToken", token)
+	req.Header.Set("token", token)
+	req.Header.Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	req.Header.Set("Pragma", "no-cache")
+	req.Header.Set("Expires", "0")
+
+	for time.Since(startTime) < retryDuration {
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println("请求失败:", err)
+			time.Sleep(retryInterval) // 请求失败时等待retryInterval后重试
+			continue
+		}
+		defer resp.Body.Close()
+
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		//log.Println("状态码:", resp.StatusCode, "响应内容:", string(bodyBytes))
+
+		// 检查是否是 "预定日期未开放" 错误
+		if strings.Contains(string(bodyBytes), "预定日期未开放，请勿超期预定") {
+			log.Printf("%s，时间：%s ：预定日期未开放，等待重试...", courtName, timeSlot)
+			time.Sleep(retryInterval) // 等待retryInterval后重试
+		} else if strings.Contains(string(bodyBytes), "已被占用") {
+			log.Printf("\"%s，时间：%s ：显示已被占用，等待重试...", courtName, timeSlot)
+			time.Sleep(retryInterval) // 等待retryInterval后重试
+		} else if strings.Contains(string(bodyBytes), "200") {
+			// 设置邮件正文
+			err := SendMessage(db.GetEmailByUserName(username), date, courtName, timeSlot, "预定成功！请及时支付")
+			if err != nil {
+				log.Println("邮件发送失败")
+			}
+			log.Printf("\"%s，时间：%s ：预定成功!", courtName, timeSlot)
+			return nil
+		} else {
+			// 如果是其他错误，直接返回
+			log.Printf("Error:%s", string(bodyBytes))
+		}
+
+		fmt.Println(string(bodyBytes))
 
 		retryInterval += time.Second
 
